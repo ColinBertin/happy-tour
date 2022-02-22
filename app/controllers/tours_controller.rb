02fe99fc939fe6d3f29ab35b7a0517e8aa60b28f
Colin Bertin <colinbertin12@gmail.com>
2022-02-22 11:45:55 +0900

    change route

---
+++ app/controllers/tours_controller.rb
@@ -0,0 +1,5 @@
+class ToursController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'tours#index'
   namespace :owner do
     resources :bookings, only: [:index]
     resources :tours, only: [:index]
