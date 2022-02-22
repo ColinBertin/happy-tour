29828f42714af7cc736603fa0529e0e09f477e02
Colin Bertin <colinbertin12@gmail.com>
2022-02-22 16:58:48 +0900

    adding pundit

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,16 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+end
